6cd2b749bba705b53b6c7d211288d569376807e8
Mel <150834946+MelsCrest@users.noreply.github.com>
2024-09-11 09:34:54 +0000

    tipos de variables

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,4 +1,21 @@
-﻿
-Console.Write("Congratulations!");
-Console.Write(" ");
-Console.Write("You wrote your first lines of code.");
+﻿/*string firstName;
+firstName = "Bob";
+Console.WriteLine(firstName);
+firstName = "Liem";
+Console.WriteLine(firstName);
+firstName = "Isabella";
+Console.WriteLine(firstName);
+firstName = "Yasmin";
+Console.WriteLine(firstName);*/
+
+/*var message = "Hola"; //se ha declarado como string
+message = 123; //está declarado como string, no ha variado
+
+var message2; //no se ha declarado el tipo de variable que es*/
+
+var name = "Bob"; //string
+var message1 = 3; //int
+var celsius = 34.4; //double
+//decimal celsius = 34.4m; //para que sepa que es decimal hay que agregar "m" al final del valor , cuando es float se agrega "f" al final del valor.
+
+Console.Write("Hello "+ name + "! You have " + message1 + " messages in your inbox. The temperature is " + celsius + " celsius.");
