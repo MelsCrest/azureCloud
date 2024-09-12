c9d2cbad72a3ee9b4f4df27aaff0f17cb3671e56
Mel <150834946+MelsCrest@users.noreply.github.com>
2024-09-12 11:50:43 +0000

    calificaciones alumnos

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,7 +1,13 @@
-var fahrenheit = 94;
-var celsius = (fahrenheit - 32m)*(5m/9m);
-Console.WriteLine("The temperature is " + Math.Round(celsius,1) + " Celsius.");
-//Console.WriteLine("The temperature is " + decimal.Round(celsius,1) + " Celsius.");
-//Console.WriteLine("The temperature is " + celsius.ToString("00.0") + " Celsius."); //Marcas cuantos caracteres quieres que se muestren
-//Console.WriteLine("The temperature is " + celsius.ToString("##.#") + " Celsius."); //Marcas cuantos caracteres quieres que se muestren
+var sofia = ((93 + 87 + 98 + 95 + 100)/5m); 
+//int[] sofia = {93, 87, 98, 95, 100};
+var nicolas = ((80 + 83 + 82 + 88 + 85)/5m);
+var zahirah = ((84 + 96 + 73 + 85 + 79)/5m);
+var jeong = ((90 + 92 + 98 + 100 + 97)/5m);
 
+Console.WriteLine($@"Student    Grade
+Sophia     {Math.Round(sofia,1)} A
+Nicolas    {Math.Round(nicolas,1)} B
+Zahirah    {Math.Round(zahirah,1)} B
+Jeong      {Math.Round(jeong,1)} A");
+
+//En la resolución del curso de Microsoft usan más variables
