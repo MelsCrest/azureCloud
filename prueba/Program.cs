21ad485fbcf441b84ff96be168c0f7a7d1e4d3b4
Melisa Cresta <melisacresta@gmail.com>
2024-09-12 10:04:00 +0200

    cambio lenguaje

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,4 +1,6 @@
-﻿//DESAFIO
+using System.Text; //importar lenguaje
+
+//DESAFIO
 string projectName = "ACME";
 string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
 Console.WriteLine($@"View English output:
@@ -14,3 +16,26 @@ Console.WriteLine($"View English output:\n\t\t{englishLocation}\n");
 string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
 string russianLocation = $@"c:\Exercise\{projectName}\ru-RU\data.txt";
 Console.WriteLine($"{russianMessage}:\n\t\t{russianLocation}\n");*/
+
+/*Console.Write("Congratulations!");
+Console.Write(" ");
+Console.Write("You wrote your first lines of code.");*/
+
+// CARACTERES CODIFICADOS
+Console.OutputEncoding = Encoding.UTF8; //cambio de ASCII a UTF8
+// Kon'nichiwa World
+Console.WriteLine("\u3053\u3093\u306B\u3061\u306F World!");
+
+// CÓDIGO PARA GENERAR FACTURAS
+Console.WriteLine("Generating invoices for customer \"Contoso Corp\" ...\n");
+Console.WriteLine("Invoice: 1021\t\tComplete!");
+Console.WriteLine("Invoice: 1022\t\tComplete!");
+Console.WriteLine("\nOutput Directory:\t");
+Console.Write(@"c:\invoices");
+
+// To generate Japanese invoices:
+// Nihon no seikyū-sho o seisei suru ni wa:
+Console.Write("\n\n\u65e5\u672c\u306e\u8acb\u6c42\u66f8\u3092\u751f\u6210\u3059\u308b\u306b\u306f\uff1a\n\t");
+// User command to run an application
+Console.WriteLine(@"c:\invoices\app.exe -j");
+
