ebf7be2a6d792ea7e1e797001ebd54ef2770abc9
Melisa Cresta <melisacresta@gmail.com>
2024-09-13 11:38:05 +0200

    llamar a metodos

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,54 +1,21 @@
-string studentName = "Sophia Johnson";
-string course1Name = "English 101";
-string course2Name = "Algebra 101";
-string course3Name = "Biology 101";
-string course4Name = "Computer Science I";
-string course5Name = "Psychology 101";
+//var dados = new Random(); //instanciar
+//Random dados = new(); //otra manera de instanciar 
+//int roll = dados.Next(1,7);
+//Console.WriteLine(roll);
 
-int course1Credit = 3;
-int course2Credit = 3;
-int course3Credit = 4;
-int course4Credit = 4;
-int course5Credit = 3;
+//El código de arriba tiene más información sobre lo que está pasando que la refactorización
 
-int gradeA = 4;
-int gradeB = 3;
+//refactorizacion
+//Console.WriteLine(new Random().Next(1,7));
 
-int course1Grade = gradeA;
-int course2Grade = gradeB;
-int course3Grade = gradeB;
-int course4Grade = gradeB;
-int course5Grade = gradeA;
 
-int totalCreditHours = 0;
+Random dice = new Random();
+int roll1 = dice.Next();
+int roll2 = dice.Next(101);
+int roll3 = dice.Next(50, 101);
 
-totalCreditHours += course1Credit;
-totalCreditHours += course2Credit;
-totalCreditHours += course3Credit;
-totalCreditHours += course4Credit;
-totalCreditHours += course5Credit;
+Console.WriteLine($"First roll: {roll1}");
+Console.WriteLine($"Second roll: {roll2}");
+Console.WriteLine($"Third roll: {roll3}");
 
-int totalGradePoints = 0;
 
-totalGradePoints += course1Credit * course1Grade;
-totalGradePoints += course2Credit * course2Grade;
-totalGradePoints += course3Credit * course3Grade;
-totalGradePoints += course4Credit * course4Grade;
-totalGradePoints += course5Credit * course5Grade;
-
-decimal gradePointAverage = (decimal) totalGradePoints/totalCreditHours;
-
-int leadingDigit = (int) gradePointAverage;
-int firstDigit = (int) (gradePointAverage * 10 ) % 10;
-int secondDigit = (int) (gradePointAverage * 100 ) % 10;
-
-Console.WriteLine($"Student: {studentName}\n");
-Console.WriteLine("Course\t\t\t\tGrade\tCredit Hours");
-
-Console.WriteLine($"{course1Name}\t\t\t{course1Grade}\t\t{course1Credit}");
-Console.WriteLine($"{course2Name}\t\t\t{course2Grade}\t\t{course2Credit}");
-Console.WriteLine($"{course3Name}\t\t\t{course3Grade}\t\t{course3Credit}");
-Console.WriteLine($"{course4Name}\t{course4Grade}\t\t{course4Credit}");
-Console.WriteLine($"{course5Name}\t\t{course5Grade}\t\t{course5Credit}");
-
-Console.WriteLine($"\nFinal GPA:\t\t\t {leadingDigit}.{firstDigit}{secondDigit}");
