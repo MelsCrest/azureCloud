fbb22d1f6ffe9db44efe745c3868e8c4b0fca29d
Melisa Cresta <melisacresta@gmail.com>
2024-09-16 12:45:49 +0200

    reto arrays

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,18 +1,7 @@
-// string[] fraudulentOrderIDs = new string[3];
+string[] numerosFraudulentos = ["B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"];
 
-// fraudulentOrderIDs[0] = "A123";
-// fraudulentOrderIDs[1] = "B456";
-// fraudulentOrderIDs[2] = "C789";
-//fraudulentOrderIDs[3] = "D000"; //si metemos una matriz más (llegando a 4) una vez declarado el tamaño de la matriz (que es 3), da un error = "System.IndexOutOfRangeException: Index was outside the bounds of the array".
-
-//Console.WriteLine(fraudulentOrderIDs[2]);
-// Console.WriteLine($@"El tamaño del array es {fraudulentOrderIDs.Length}");
-
-// for (int i = 0; i < fraudulentOrderIDs.Length; i++){ //para mostrar la longitud del array
-//     Console.WriteLine(fraudulentOrderIDs[i]);
-// };
-
-string[] fraudulentOrderIDs = [ "A123", "B456", "C789" ];
-foreach (var item in fraudulentOrderIDs){ //recorrer un array con foreach
-    Console.WriteLine(item);
+foreach (string numero in numerosFraudulentos){
+    if(numero.StartsWith("B")){
+        Console.WriteLine(numero);
+    }
 }
