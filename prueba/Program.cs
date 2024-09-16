e921fcb9f2c4d5de519135610ca425d526f633f2
Melisa Cresta <melisacresta@gmail.com>
2024-09-16 13:54:15 +0200

    reto arrays 2

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,7 +1,40 @@
-string[] numerosFraudulentos = ["B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"];
+// initialize variables - graded assignments 
+int currentAssignments = 5;
 
-foreach (string numero in numerosFraudulentos){
-    if(numero.StartsWith("B")){
-        Console.WriteLine(numero);
-    }
-}
+string[] alumnos = ["Sophia", "Andrew", "Emma", "Logan"];
+
+int[] sophia = {90, 86, 87, 98, 100};
+int[] andrew = {92, 89, 81, 96, 90};
+int[] emma = {90, 85, 87, 98, 68};
+int[] logan = {90, 95, 87, 88, 96};
+
+
+
+// int sophiaSum = 0;
+// int andrewSum = 0;
+// int emmaSum = 0;
+// int loganSum = 0;
+
+// decimal sophiaScore;
+// decimal andrewScore;
+// decimal emmaScore;
+// decimal loganScore;
+
+// sophiaSum = sophia1 + sophia2 + sophia3 + sophia4 + sophia5;
+// andrewSum = andrew1 + andrew2 + andrew3 + andrew4 + andrew5;
+// emmaSum = emma1 + emma2 + emma3 + emma4 + emma5;
+// loganSum = logan1 + logan2 + logan3 + logan4 + logan5;
+
+// sophiaScore = (decimal)sophiaSum / currentAssignments;
+// andrewScore = (decimal)andrewSum / currentAssignments;
+// emmaScore = (decimal)emmaSum / currentAssignments;
+// loganScore = (decimal)loganSum / currentAssignments;
+
+// Console.WriteLine("Student\t\tGrade\n");
+// Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
+// Console.WriteLine("Andrew:\t\t" + andrewScore + "\tB+");
+// Console.WriteLine("Emma:\t\t" + emmaScore + "\tB");
+// Console.WriteLine("Logan:\t\t" + loganScore + "\tA-");
+
+// Console.WriteLine("Press the Enter key to continue");
+// Console.ReadLine();
