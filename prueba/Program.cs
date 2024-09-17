18af35d5e1b979015d9c103d59021d7a417c3d8e
Melisa Cresta <melisacresta@gmail.com>
2024-09-17 13:12:27 +0200

    cara o cruz

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,33 +1,8 @@
-// //operador igualdad ==
-// Console.WriteLine("a" == "a");
-// Console.WriteLine("a" == "A");
-// Console.WriteLine(1 == 2);
-
-// string myValue = "a";
-// Console.WriteLine(myValue == "a");
-/* ************************ */
-// string value1 = " a";
-// string value2 = "A ";
-// Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
-
-/* ************************ */
-// //operador desigual !=
-// Console.WriteLine("a" != "a");
-// Console.WriteLine("a" != "A");
-// Console.WriteLine(1 != 2);
-
-// string myValue2 = "a";
-// Console.WriteLine(myValue2 != "a");
-
-/* ******************** */
-// //negacion lógica !var
-// string pangram = "The quick brown fox jumps over the lazy dog.";
-// Console.WriteLine(!pangram.Contains("fox"));
-// Console.WriteLine(!pangram.Contains("cow"));
-
-/* ******************** */
-//operador ternario
-int saleAmount = 1001;
-int discount = saleAmount > 1000 ? 100 : 50;
-// si el saldo es mayor a 1000 imprime 100 sino imprime 50 
-Console.WriteLine($"Discount: {discount}");
+// Random coin = new Random();
+// int flip = coin.Next(0, 2);
+// Console.WriteLine((flip == 0) ? "heads" : "tails");
+// /* ******************** */
+// Random moneda = new Random();
+// Console.WriteLine((moneda.Next(0, 2) == 0) ? "heads" : "tails");
+// /* ******************** */
+Console.WriteLine(new Random().Next(0,2) < 1 ? "cara" : "cruz");Console.ReadKey();
