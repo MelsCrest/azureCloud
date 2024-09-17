bfec0ab58d22d978986b09fe4cb151ca873dd1d8
Melisa Cresta <melisacresta@gmail.com>
2024-09-17 11:04:37 +0200

    reto array meter alumnos

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,12 +1,13 @@
 //Crear un array de nombres
-string[] alumnos = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
+string[] alumnos = new string[] { "Sophia", "Andrew", "Emma", "Logan", "Laura" };
 
 //Crear un array de arrays de enteros
-int[][] notas = new int[4][];
+int[][] notas = new int[alumnos.Length][];
 notas[0] = new int[] { 90, 86, 87, 98, 100};
 notas[1] = new int[] { 92, 89, 81, 96, 90};
 notas[2] = new int[] { 90, 85, 87, 98, 68};
 notas[3] = new int[] { 90, 95, 87, 88, 96};
+notas[4] = new int[] { 92, 77, 90, 80, 91};
 
 var indiceAlumno = 0;
 decimal sumaValores = 0;
@@ -21,7 +22,7 @@ foreach (var alumno in alumnos){
         // foreach (var nota in notas[indiceAlumno]){
         //  sumaValores = sumaValores + nota;   
         // }
-    var media = sumaValores / (notas.Length + 1);
+    var media = sumaValores / (notas[0].Length);
     indiceAlumno++;
     sumaValores = 0;
     var notaLetra = "";
