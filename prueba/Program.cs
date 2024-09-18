d6bbbafc99d86adba7e5eac9b8ae888fc616753d
Melisa Cresta <melisacresta@gmail.com>
2024-09-18 13:18:35 +0200

    ejercicio juego

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,21 +1,44 @@
-Console.Clear();
-Random random = new Random();
-int current = random.Next(1, 11);
+// Random dado = new Random();
+// int heroe = 10;st
+// int monstruo = 10;
+// int tirada = 0;
 
-/*
-do
-{
-    current = random.Next(1, 11);
-    if(current >= 8){ 
-    continue
-    };
-    Console.WriteLine(current);
-} while (current != 7);
-*/
+// do
+// {
+//     //tirada del héroe
+//     tirada = dado.Next(1, 11);
+//     monstruo -= tirada;
+//     Console.WriteLine($@"El monstruo ha sido herido y ha perdido {tirada} de salud y ahora tiene {monstruo} de vida.");
 
-while (current >= 3)
-{
-    Console.WriteLine(current);
-    current = random.Next(1, 11);
-}
-Console.WriteLine($"Last number: {current}");
+//     if(monstruo <= 0){ 
+//     continue;
+//     };
+
+//     //tirada del monstruo
+//     tirada = dado.Next(1, 11);
+//     heroe -= tirada;
+//     Console.WriteLine($@"El héroe ha sido herido y ha perdido {tirada} de salud y ahora tiene {heroe} de vida.");
+
+// } while (heroe > 0 && monruo > 0);
+
+// Console.WriteLine(heroe > monstruo ? "El héroe gana!" : "El monstruo gana!");
+
+//OPCIÓN CON FLAGS
+Random dado = new Random();
+int heroe = 10;
+int monstruo = 10;
+int tirada = 0;
+var isHero = true;
+
+do{
+    tirada = dado.Next(1,11);
+    if(isHero){
+        monstruo-= tirada;
+        Console.WriteLine($@"El monstruo ha sido herido y ha perdido {tirada} de salud y ahora tiene {monstruo} de vida.");
+    } else {
+        heroe-= tirada;
+        Console.WriteLine($@"El héroe ha sido herido y ha perdido {tirada} de salud y ahora tiene {heroe} de vida.");
+    }
+    isHero = !isHero;
+}while (heroe > 0 && monstruo > 0);
+Console.WriteLine(heroe > monstruo? "El héroe gana!" : "El monstruo gana!");
