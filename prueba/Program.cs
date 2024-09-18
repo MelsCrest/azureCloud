8d8ae1e0d10fb77cff8602ae43fa899168d8a929
Melisa Cresta <melisacresta@gmail.com>
2024-09-18 13:43:08 +0200

    datos de variables

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,107 +1,22 @@
-// string? readResult; //string? acepta valores NULL es habitual su uso con un Console.ReadLine();
-// Console.WriteLine("Enter a string:");
-// do
-// {
-//     readResult = Console.ReadLine();
-// } while (readResult == null);
+// Console.WriteLine("Signed integral types:");
 
-// //Proyecto 1
-// string? readResult;
-// string valueEntered = "";
-// int numValue = 0;
-// bool validNumber = false;
+// Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
+// Console.WriteLine($"short  : {short.MinValue} to {short.MaxValue}");
+// Console.WriteLine($"int    : {int.MinValue} to {int.MaxValue}");
+// Console.WriteLine($"long   : {long.MinValue} to {long.MaxValue}");
+// Console.WriteLine($"double   : {double.MinValue} to {double.MaxValue}");
+ /* *************** */
+// Console.WriteLine("");
+// Console.WriteLine("Unsigned integral types:");
 
-// Console.WriteLine("Enter an integer value between 5 and 10");
+// Console.WriteLine($"byte   : {byte.MinValue} to {byte.MaxValue}");
+// Console.WriteLine($"ushort : {ushort.MinValue} to {ushort.MaxValue}");
+// Console.WriteLine($"uint   : {uint.MinValue} to {uint.MaxValue}");
+// Console.WriteLine($"ulong  : {ulong.MinValue} to {ulong.MaxValue}");
 
-// do
-// {
-//     readResult = Console.ReadLine();
-//     if (readResult != null) 
-//     {
-//         valueEntered = readResult;
-//     }
-
-//     validNumber = int.TryParse(valueEntered, out numValue);
-
-//     if (validNumber == true)
-//     {
-//         if (numValue <= 5 || numValue >= 10)
-//         {
-//             validNumber = false;
-//             Console.WriteLine($"You entered {numValue}. Please enter a number between 5 and 10.");
-//         }
-//     }
-//     else 
-//     {
-//         Console.WriteLine("Sorry, you entered an invalid number, please try again");
-//     }
-// } while (validNumber == false);
-
-// Console.WriteLine($"Your input value ({numValue}) has been accepted.");
-
-// readResult = Console.ReadLine();
-
-// //Proyecto 2
-// string? readResult;
-// string roleName = "";
-// bool validEntry = false;
-
-// do
-// {                
-//     Console.WriteLine("Enter your role name (Administrator, Manager, or User)");
-//     readResult = Console.ReadLine();
-//     if (readResult != null) 
-//     {
-//         roleName = readResult.Trim();
-//     }
-
-//     if (roleName.ToLower() == "administrator" || roleName.ToLower() == "manager" || roleName.ToLower() == "user") 
-//     {
-//         validEntry = true;
-//     }
-//     else
-//     {
-//         Console.Write($"The role name that you entered, \"{roleName}\" is not valid. ");
-//     }
-
-// } while (validEntry == false);
-
-// Console.WriteLine($"Your input value ({roleName}) has been accepted.");
-// readResult = Console.ReadLine();
-
-//Proyecto 3
-string[] myStrings = new string[2] { "I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices" };
-int stringsCount = myStrings.Length;
-
-string myString = "";
-int periodLocation = 0;
-
-for (int i = 0; i < stringsCount; i++)
-{
-    myString = myStrings[i];
-    periodLocation = myString.IndexOf(".");
-
-    string mySentence;
-
-    // extract sentences from each string and display them one at a time
-    while (periodLocation != -1)
-    {
-
-        // first sentence is the string value to the left of the period location
-        mySentence = myString.Remove(periodLocation);
-
-        // the remainder of myString is the string value to the right of the location
-        myString = myString.Substring(periodLocation + 1);
-
-        // remove any leading white-space from myString
-        myString = myString.TrimStart();
-
-        // update the comma location and increment the counter
-        periodLocation = myString.IndexOf(".");
-
-        Console.WriteLine(mySentence);
-    }
- 
-    mySentence = myString.Trim();
-    Console.WriteLine(mySentence);
-}
+/* ********************* */
+Console.WriteLine("");
+Console.WriteLine("Floating point types:");
+Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
+Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
+Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
