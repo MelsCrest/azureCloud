1549c573e7b72e04d3d2bc4e9441d7e2a7fbdaae
Melisa Cresta <melisacresta@gmail.com>
2024-09-18 13:26:32 +0200

    Proyectos 1,2 y 3 bucles

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,44 +1,107 @@
-// Random dado = new Random();
-// int heroe = 10;st
-// int monstruo = 10;
-// int tirada = 0;
+// string? readResult; //string? acepta valores NULL es habitual su uso con un Console.ReadLine();
+// Console.WriteLine("Enter a string:");
+// do
+// {
+//     readResult = Console.ReadLine();
+// } while (readResult == null);
+
+// //Proyecto 1
+// string? readResult;
+// string valueEntered = "";
+// int numValue = 0;
+// bool validNumber = false;
+
+// Console.WriteLine("Enter an integer value between 5 and 10");
 
 // do
 // {
-//     //tirada del héroe
-//     tirada = dado.Next(1, 11);
-//     monstruo -= tirada;
-//     Console.WriteLine($@"El monstruo ha sido herido y ha perdido {tirada} de salud y ahora tiene {monstruo} de vida.");
+//     readResult = Console.ReadLine();
+//     if (readResult != null) 
+//     {
+//         valueEntered = readResult;
+//     }
 
-//     if(monstruo <= 0){ 
-//     continue;
-//     };
+//     validNumber = int.TryParse(valueEntered, out numValue);
 
-//     //tirada del monstruo
-//     tirada = dado.Next(1, 11);
-//     heroe -= tirada;
-//     Console.WriteLine($@"El héroe ha sido herido y ha perdido {tirada} de salud y ahora tiene {heroe} de vida.");
+//     if (validNumber == true)
+//     {
+//         if (numValue <= 5 || numValue >= 10)
+//         {
+//             validNumber = false;
+//             Console.WriteLine($"You entered {numValue}. Please enter a number between 5 and 10.");
+//         }
+//     }
+//     else 
+//     {
+//         Console.WriteLine("Sorry, you entered an invalid number, please try again");
+//     }
+// } while (validNumber == false);
 
-// } while (heroe > 0 && monruo > 0);
+// Console.WriteLine($"Your input value ({numValue}) has been accepted.");
 
-// Console.WriteLine(heroe > monstruo ? "El héroe gana!" : "El monstruo gana!");
+// readResult = Console.ReadLine();
 
-//OPCIÓN CON FLAGS
-Random dado = new Random();
-int heroe = 10;
-int monstruo = 10;
-int tirada = 0;
-var isHero = true;
+// //Proyecto 2
+// string? readResult;
+// string roleName = "";
+// bool validEntry = false;
 
-do{
-    tirada = dado.Next(1,11);
-    if(isHero){
-        monstruo-= tirada;
-        Console.WriteLine($@"El monstruo ha sido herido y ha perdido {tirada} de salud y ahora tiene {monstruo} de vida.");
-    } else {
-        heroe-= tirada;
-        Console.WriteLine($@"El héroe ha sido herido y ha perdido {tirada} de salud y ahora tiene {heroe} de vida.");
+// do
+// {                
+//     Console.WriteLine("Enter your role name (Administrator, Manager, or User)");
+//     readResult = Console.ReadLine();
+//     if (readResult != null) 
+//     {
+//         roleName = readResult.Trim();
+//     }
+
+//     if (roleName.ToLower() == "administrator" || roleName.ToLower() == "manager" || roleName.ToLower() == "user") 
+//     {
+//         validEntry = true;
+//     }
+//     else
+//     {
+//         Console.Write($"The role name that you entered, \"{roleName}\" is not valid. ");
+//     }
+
+// } while (validEntry == false);
+
+// Console.WriteLine($"Your input value ({roleName}) has been accepted.");
+// readResult = Console.ReadLine();
+
+//Proyecto 3
+string[] myStrings = new string[2] { "I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices" };
+int stringsCount = myStrings.Length;
+
+string myString = "";
+int periodLocation = 0;
+
+for (int i = 0; i < stringsCount; i++)
+{
+    myString = myStrings[i];
+    periodLocation = myString.IndexOf(".");
+
+    string mySentence;
+
+    // extract sentences from each string and display them one at a time
+    while (periodLocation != -1)
+    {
+
+        // first sentence is the string value to the left of the period location
+        mySentence = myString.Remove(periodLocation);
+
+        // the remainder of myString is the string value to the right of the location
+        myString = myString.Substring(periodLocation + 1);
+
+        // remove any leading white-space from myString
+        myString = myString.TrimStart();
+
+        // update the comma location and increment the counter
+        periodLocation = myString.IndexOf(".");
+
+        Console.WriteLine(mySentence);
     }
-    isHero = !isHero;
-}while (heroe > 0 && monstruo > 0);
-Console.WriteLine(heroe > monstruo? "El héroe gana!" : "El monstruo gana!");
+ 
+    mySentence = myString.Trim();
+    Console.WriteLine(mySentence);
+}
