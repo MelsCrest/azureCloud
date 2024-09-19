2efdb1a7b6e47650b3294640cae2428fc667b065
Melisa Cresta <melisacresta@gmail.com>
2024-09-19 13:56:38 +0200

    reto palabras inversas

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,14 +1,27 @@
-string value = "abc123";
-char[] valueArray = value.ToCharArray(); //Convierte un string en caracteres
-Array.Reverse(valueArray);
-// string result = new string(valueArray);
-string result = String.Join(",", valueArray); 
-//.Join(caracter, array) Añade un caracter después de cada valor del array, como si hiciera un bucle
-Console.WriteLine(result);
+string pangram = "The quick brown fox jumps over the lazy dog";
 
-string[] items = result.Split(',');
-//.Split(caracter) Se utiliza el caracter como divisor del array (elemento que parte el array)
-foreach (string item in items)
-{
-    Console.WriteLine(item);
+// string[] mensaje = pangram.Split(' '); //Separa por palabras
+// string[] newMensaje = new string[mensaje.Length];
+
+// for (int i = 0; i < mensaje.Length; i++){
+//     char[] letras = mensaje[i].ToCharArray();
+//     Array.Reverse(letras);
+//     newMensaje[i] = new string(letras);
+// }
+
+// string resultado = String.Join(" ", newMensaje);
+// Console.WriteLine(resultado);
+
+/* ******************************* */
+string[] resultado = pangram.Split(" ");
+string[] reverse = new string[resultado.Length];
+string final = string.Empty;
+
+for (int i = 0; i < resultado.Length; i++){
+    // string palabra = resultado[i];
+    // string palabraInvertida = string.Join("", palabra.ToCharArray().Reverse());
+    // reverse[i] = palabraInvertida;
+    final += string.Join("", resultado[i].ToCharArray().Reverse()) + " ";
 }
+   //Console.WriteLine(string.Join(" ", reverse));
+   Console.WriteLine(final);
