71e63d248c7d51818d182655e8cc042261f2ae32
Mel <150834946+MelsCrest@users.noreply.github.com>
2024-09-19 19:01:47 +0000

    ejercicio pedidos

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,27 +1,7 @@
-string pangram = "The quick brown fox jumps over the lazy dog";
+﻿string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+string[] orderIDs = orderStream.Split(",");
+Array.Sort(orderIDs);
 
-// string[] mensaje = pangram.Split(' '); //Separa por palabras
-// string[] newMensaje = new string[mensaje.Length];
-
-// for (int i = 0; i < mensaje.Length; i++){
-//     char[] letras = mensaje[i].ToCharArray();
-//     Array.Reverse(letras);
-//     newMensaje[i] = new string(letras);
-// }
-
-// string resultado = String.Join(" ", newMensaje);
-// Console.WriteLine(resultado);
-
-/* ******************************* */
-string[] resultado = pangram.Split(" ");
-string[] reverse = new string[resultado.Length];
-string final = string.Empty;
-
-for (int i = 0; i < resultado.Length; i++){
-    // string palabra = resultado[i];
-    // string palabraInvertida = string.Join("", palabra.ToCharArray().Reverse());
-    // reverse[i] = palabraInvertida;
-    final += string.Join("", resultado[i].ToCharArray().Reverse()) + " ";
+foreach(var order in orderIDs){  
+    Console.WriteLine(order.Length != 4 ? $@"{order}    - Error": order);
 }
-   //Console.WriteLine(string.Join(" ", reverse));
-   Console.WriteLine(final);
