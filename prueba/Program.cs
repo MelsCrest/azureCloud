bdb5327a471089f7beeb520bf3dd19a3c753853b
Melisa Cresta <melisacresta@gmail.com>
2024-09-19 10:22:35 +0200

    valores de las variables

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,20 +1,22 @@
-// Console.WriteLine("Signed integral types:");
+//Muestra el mínimo y el máximo
+Console.WriteLine("Signed integral types:");
 
-// Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
-// Console.WriteLine($"short  : {short.MinValue} to {short.MaxValue}");
-// Console.WriteLine($"int    : {int.MinValue} to {int.MaxValue}");
-// Console.WriteLine($"long   : {long.MinValue} to {long.MaxValue}");
-// Console.WriteLine($"double   : {double.MinValue} to {double.MaxValue}");
+Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
+Console.WriteLine($"short  : {short.MinValue} to {short.MaxValue}");
+Console.WriteLine($"int    : {int.MinValue} to {int.MaxValue}");
+Console.WriteLine($"long   : {long.MinValue} to {long.MaxValue}");
  /* *************** */
-// Console.WriteLine("");
-// Console.WriteLine("Unsigned integral types:");
+//Muestra los valores desde 0
+Console.WriteLine("");
+Console.WriteLine("Unsigned integral types:");
 
-// Console.WriteLine($"byte   : {byte.MinValue} to {byte.MaxValue}");
-// Console.WriteLine($"ushort : {ushort.MinValue} to {ushort.MaxValue}");
-// Console.WriteLine($"uint   : {uint.MinValue} to {uint.MaxValue}");
-// Console.WriteLine($"ulong  : {ulong.MinValue} to {ulong.MaxValue}");
+Console.WriteLine($"byte   : {byte.MinValue} to {byte.MaxValue}");
+Console.WriteLine($"ushort : {ushort.MinValue} to {ushort.MaxValue}");
+Console.WriteLine($"uint   : {uint.MinValue} to {uint.MaxValue}");
+Console.WriteLine($"ulong  : {ulong.MinValue} to {ulong.MaxValue}");
 
 /* ********************* */
+//El mínimo y el máximo de float, double, decimal
 Console.WriteLine("");
 Console.WriteLine("Floating point types:");
 Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
