7ada90cd42a31fa476bf8d5e80eba9905877b4e5
Melisa Cresta <melisacresta@gmail.com>
2024-09-19 11:33:53 +0200

    conversion de tipos de datos

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,24 +1,54 @@
-//Muestra el mínimo y el máximo
-Console.WriteLine("Signed integral types:");
+//el resultado es un string y los valores se concatenan
+// int first = 2;
+// string second = "4";
+// string result = first + second;
+// Console.WriteLine(result);
 
-Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
-Console.WriteLine($"short  : {short.MinValue} to {short.MaxValue}");
-Console.WriteLine($"int    : {int.MinValue} to {int.MaxValue}");
-Console.WriteLine($"long   : {long.MinValue} to {long.MaxValue}");
- /* *************** */
-//Muestra los valores desde 0
-Console.WriteLine("");
-Console.WriteLine("Unsigned integral types:");
+//el resultado es un int y no puede sumar los valores al no ser numéricos ambos.
+// int first = 2;
+// string second = "4";
+// int result = first + second;
+// Console.WriteLine(result);
 
-Console.WriteLine($"byte   : {byte.MinValue} to {byte.MaxValue}");
-Console.WriteLine($"ushort : {ushort.MinValue} to {ushort.MaxValue}");
-Console.WriteLine($"uint   : {uint.MinValue} to {uint.MaxValue}");
-Console.WriteLine($"ulong  : {ulong.MinValue} to {ulong.MaxValue}");
+//Con int.Parse hacemos una conversión explícita de second
+// int first = 2;
+// string second = "4";
+// int result = first + int.Parse(second);
+// Console.WriteLine(result);
 
-/* ********************* */
-//El mínimo y el máximo de float, double, decimal
-Console.WriteLine("");
-Console.WriteLine("Floating point types:");
-Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
-Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
-Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
+//Con .toString() convertimos valores numericos a cadena de texto
+// int first = 5;
+// int second = 7;
+// string message = first.ToString() + second.ToString();
+// Console.WriteLine(message);
+
+//Con int.Parse(var) convertimos una cadena de texto en valor numérico
+// string first = "5";
+// string second = "7";
+// int sum = int.Parse(first) + int.Parse(second);
+// Console.WriteLine(sum);
+
+//Con try catch evaluar errores. El try catch es muy pesado y rompe el flujo
+// try{
+// string first = "5";
+// string second = "a";
+// int sum = int.Parse(first) + int.Parse(second);
+// Console.WriteLine(sum);
+// } 
+// catch(SystemException){
+//     Console.WriteLine("Número no válido");
+// }
+
+//.TryParse(var, out nueva variable). Cuando puede convertir el valor "devuelve" true y hace la suma de los valores, sino false el valor que no se puede cambiar tiene valor 0.
+// string first = "5";
+// string second = "a";
+// int resultado;
+// Console.WriteLine(int.TryParse(second, out resultado)); //mostramos el true o false, sino lo imprimimos lo hace por debajo
+// int sum = int.Parse(first) + resultado;
+// Console.WriteLine(sum);
+
+//Convert.ToInt32
+string value1 = "5";
+string value2 = "7";
+int result = Convert.ToInt32(value1) * Convert.ToInt32(value2);
+Console.WriteLine(result);
