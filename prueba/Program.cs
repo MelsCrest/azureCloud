91ebed9e237532e72e76edae51716708193c0a78
Melisa Cresta <melisacresta@gmail.com>
2024-09-19 12:31:52 +0200

    desafio valores

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,16 +1,13 @@
-string[] values = {"12,3", "45", "ABC", "11", "DEF"}; //fijarse que en vez de punto pongamos coma para que lo reconozca
-var letras ="";
-var totalNum=0.0;
+int value1 = 11;
+decimal value2 = 6.2m;
+float value3 = 4.3f;
 
-foreach (var valor in values){
-    
-    if (double.TryParse(valor,out double num)){
-        totalNum += num;
-    }else{
-        letras += valor;
-    }
-}
+int result1 = Convert.ToInt32(value1 / value2);
+decimal result2 = value2 / (decimal)value3;
+float result3 = value3 / value1;
 
-Console.WriteLine($"Letras: {letras}");
-Console.WriteLine($"Total: {totalNum}");
+Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
 
+Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+
+Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
