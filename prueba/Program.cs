0f270250724ee8d9aecd431e9d13c95fb5957c44
Melisa Cresta <melisacresta@gmail.com>
2024-09-19 13:29:25 +0200

    métodos ToCharArray, join y split

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,36 +1,14 @@
-//Clear(array, inicio, cuanto a borrar) borra los valores almacenados en los elementos del array, pero no el espacio en el array
-//Dependiendo del tipo de array al espacio vacío devolverá cadena vacía, 0, false, null...
-// string[] pallets = { "B14", "A11", "B12", "A13" };
-// Console.WriteLine("");
+string value = "abc123";
+char[] valueArray = value.ToCharArray(); //Convierte un string en caracteres
+Array.Reverse(valueArray);
+// string result = new string(valueArray);
+string result = String.Join(",", valueArray); 
+//.Join(caracter, array) Añade un caracter después de cada valor del array, como si hiciera un bucle
+Console.WriteLine(result);
 
-// Console.WriteLine($"Before: {pallets[0]}");
-// Array.Clear(pallets, 0, 2);
-// Console.WriteLine($"After: {pallets[0]}");
-
-// Console.WriteLine($"Clearing 2 ... count: {pallets.Length}"); //sigue siendo un array de 4 espacios aunque se le hayan quitado los valores primero y segundo
-// foreach (var pallet in pallets)
-// {
-//     Console.WriteLine($"-- {pallet}");
-// }
-//Resize(ref array, número de espacios total en la variable) para variar (aumentar, disminuir) el tamaño de un array
-string[] pallets = { "B14", "A11", "B12", "A13" };
-Console.WriteLine("");
-
-Array.Clear(pallets, 0, 2);
-Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");//hay 4 espacios los dos primeros sin informacion
-foreach (var pallet in pallets)
+string[] items = result.Split(',');
+//.Split(caracter) Se utiliza el caracter como divisor del array (elemento que parte el array)
+foreach (string item in items)
 {
-    Console.WriteLine($"-- {pallet}");
-}
-
-Console.WriteLine("");
-Array.Resize(ref pallets, 6); //aumentamos el tamaño de los espacios de 4 a 6.
-Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
-
-pallets[4] = "C01";
-pallets[5] = "C02";
-
-foreach (var pallet in pallets)
-{
-    Console.WriteLine($"-- {pallet}");
+    Console.WriteLine(item);
 }
