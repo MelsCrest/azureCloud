fc17c5b30fa57e8c1127e726f60bec289d29567f
Melisa Cresta <melisacresta@gmail.com>
2024-09-20 13:27:56 +0200

    ejercicio metodo de tipo de datos en cadena integrados

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,56 +1,10 @@
-﻿// INDEXOF() - .IndexOf('caracter') - Mostrará la posición en la cadena de ese caracter. Empezará a contar desde 0.
-// string message = "Find what is (inside the parentheses)";
+﻿const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
 
-// int openingPosition = message.IndexOf('(');
-// int closingPosition = message.IndexOf(')');
+string quantity = input.Substring(input.IndexOf("5000"),4);
+string output = input.Substring(input.IndexOf("<div>")+5,input.Length - 11).Replace("&trade", "&reg");
 
-// Console.WriteLine(openingPosition);
-// Console.WriteLine(closingPosition);
-/* ************************************ */
+Console.WriteLine(quantity);
+Console.WriteLine(output);
 
-// SUBSTRING() - .Substring(var, var); - Mostrará lo que se encuentre dentro de esas variables que contienen .IndexOf() que indican la posición.
-// string message = "Find what is (inside the parentheses)";
-
-// int openingPosition = message.IndexOf('(');
-// int closingPosition = message.IndexOf(')');
-
-// openingPosition += 1;
-
-// int length = closingPosition - openingPosition;
-// Console.WriteLine(message.Substring(openingPosition, length));
-
-/* ************************************ */
-// INDEXOF() Y SUBSTRING()
-// string message = "What is the value <span>between the tags</span>?";
-
-// const string openSpan = "<span>";
-// const string closeSpan = "</span>";
-
-// int openingPosition = message.IndexOf(openSpan);
-// int closingPosition = message.IndexOf(closeSpan);
-
-// openingPosition += openSpan.Length;
-// int length = closingPosition - openingPosition;
-// Console.WriteLine(message.Substring(openingPosition, length));
-
-/* ************************************ */
-// INDEXOF() Y LASTINDEXOF() - IndexOf - LastIndexOf -- Primer y ultimo caracter
-// string message = "hello there!";
-
-// int first_h = message.IndexOf('h');
-// int last_h = message.LastIndexOf('h');
-
-// Console.WriteLine($"For the message: '{message}', the first 'h' is at position {first_h} and the last 'h' is at position {last_h}.");
-
-/* ************************************ */
-// REMOVE() - Remove(inicio, final) - Elimina todo aquello que se encuentre dentro de los valores indicados como parámetros. La longitud a borrar a de coincidir con el tamaño del string
-// string data = "12345John Smith          5000  3  ";
-// string updatedData = data.Remove(5, 20);
-// Console.WriteLine(updatedData);
-
-/* ************************************ */
-// REPLACE() - Replace("esto", "por esto") - Reemplaza un dato que indicamos como primer parámetro por el dato que indicamos como segundo parámetro
-string message = "This--is--ex-amp-le--da-ta";
-message = message.Replace("--", " ");
-message = message.Replace("-", "");
-Console.WriteLine(message);
+//IndexOf() le permitía identificar la posición de un carácter o cadena dentro de otra cadena.
+//IndexOfAny() para buscar la posición de cualquier valor de una matriz de char especificada
