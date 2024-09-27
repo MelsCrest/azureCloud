e4bc8b642477a34964a4e06d9f5a55bffe6c2bd6
Mel <150834946+MelsCrest@users.noreply.github.com>
2024-09-27 11:07:34 +0200

    método numeros random

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,36 +1,10 @@
-﻿//-----------  METODOS  -----------
-// fechas ();
-// void fechas(){
-//     var fecha = new DateTime(2024, 05, 31);
-//     Console.WriteLine (fecha.AddMonths(1).Ticks);
-//     Thread.Sleep(1000);
-//     fecha = new DateTime(01, 05, 2024);
-// }
-/* ***************************** */
-// for(int i = 0; i < 3; i++){
-//     Console.WriteLine(HolaMundo());
-// }
+﻿Console.WriteLine("Generating random numbers:");
+DisplayRandomNumbers();
 
-// string HolaMundo(){
-//     return "Hola Mundo";
-// }
-/* ***************************** */
-// int[] numeros = {1,2,3,4,5};
-
-// Console.WriteLine("Contents of Array:");
-// PrintArray();
-
-// void PrintArray()
-// {
-//     foreach (var numero in numeros)
-//     {
-//         Console.Write($"{numero} ");
-//     }
-//     Console.WriteLine();
-// }
-
-//Independientemente de donde coloquemos el método, se le llamará primero. Pero primero se declara el array, si o si.
-//Los métodos es mejor que empiecen con mayúscula.
-
-// void ShowData(string a, int b, int c); // Menos claro
-// void DisplayDate(string month, int day, int year); //Más claro
+void DisplayRandomNumbers(){
+    Random random= new Random();
+    for(int i=0; i<5; i++){
+        Console.WriteLine($"{random.Next(1,100)}");
+    }
+    Console.WriteLine();
+}
