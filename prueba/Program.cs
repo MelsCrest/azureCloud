43841e6bc0d527f02acb51704610170eb873830c
Mel <150834946+MelsCrest@users.noreply.github.com>
2024-09-27 13:43:27 +0200

    ámbito de un método

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,26 +1,68 @@
-﻿int[] schedule = { 800, 1200, 1600, 2000 };
+﻿// string[] students = {"Jenna", "Ayesha", "Carlos", "Viktor"};
 
-DisplayAdjustedTimes(schedule, 6, -6);
+// DisplayStudents(students);
+// DisplayStudents(new string[] {"Robert","Vanya"});
 
-void DisplayAdjustedTimes(int[] times, int currentGMT, int newGMT)
+// void DisplayStudents(string[] students) 
+// {
+//     foreach (string student in students) 
+//     {
+//         Console.Write($"{student}, ");
+//     }
+//     Console.WriteLine();
+// }
+
+// PrintCircleArea(12);
+
+// void PrintCircleArea(int radius)
+// {
+//     double pi = 3.14159;
+//     double area = pi * (radius * radius);
+//     Console.WriteLine($"Area = {area}");
+// }
+/* ************************** */
+// PrintCircleArea(12);
+
+// void PrintCircleArea(int radius)
+// {
+//     double pi = 3.14159;
+//     double area = pi * (radius * radius);
+//     Console.WriteLine($"Area = {area}");
+// }
+
+//Aparecen mensajes de error informando de que los nombres pi y radius no existen en el ámbito actual. Esas variables solo existen dentro del ámbito del método PrintCircleArea.
+// PrintCircleArea(12); 
+// double circumference = 2 * pi * radius;
+
+/* ***************** */
+// void PrintCircleCircumference(int radius)
+// {
+//     double pi = 3.14159;
+//     double circumference = 2 * pi * radius;
+//     Console.WriteLine($"Circumference = {circumference}");
+// }
+
+/* ***************** */
+double pi = 3.14159;
+
+void PrintCircleArea(int radius)
 {
-    int diff = 0;
-    if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12)
-    {
-        Console.WriteLine("Invalid GMT");
-    }
-    else if (newGMT <= 0 && currentGMT <= 0 || newGMT >= 0 && currentGMT >= 0)
-    {
-        diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
-    }
-    else
-    {
-        diff = 100 * (Math.Abs(newGMT) + Math.Abs(currentGMT));
-    }
+    double area = pi * (radius * radius);
+    Console.WriteLine($"Area = {area}");
+}
 
-    for (int i = 0; i < times.Length; i++)
-    {
-        int newTime = ((times[i] + diff)) % 2400;
-        Console.WriteLine($"{times[i]} -> {newTime}");
-    }
+void PrintCircleCircumference(int radius)
+{
+    double circumference = 2 * pi * radius;
+    Console.WriteLine($"Circumference = {circumference}");
+}
+
+PrintCircleInfo(12);
+PrintCircleInfo(24);
+
+void PrintCircleInfo(int radius) 
+{
+    Console.WriteLine($"Circle with radius {radius}");
+    PrintCircleArea(radius);
+    PrintCircleCircumference(radius);
 }
