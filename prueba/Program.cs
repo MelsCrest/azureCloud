ed40dc75274f51a5c458369916c3e6f07936371c
Melisa Cresta <melisacresta@gmail.com>
2024-10-02 11:25:38 +0200

    depurador1

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,12 +1,19 @@
-﻿Random random = new Random(); //instanciar la clase Random
-numerosRandom();
+﻿string[] students = new string[] {"Sophia", "Nicolas", "Zahirah", "Jeong"};
 
-void numerosRandom(){
-    for (int i = 0; i < 5; i++){
-        // int numero = random.Next(1, 100);
-        // Console.Write(numero + " ");
-        Console.Write(random.Next(1, 100) + " ");
-    }
-    Console.WriteLine();
+int studentCount = students.Length;
+
+//Console.WriteLine("The final name is: " + students[studentCount]); //Al ejecutar dará error porque la longitud del array es mayor que los elementos, uno empieza por 1, el otro empieza por 0.
+
+//Console.WriteLine("The final name is: " + students[studentCount - 1]); //para empezar a recorrer el array desde 0 y no 1
+
+//Se puede usar un try catch para evaluar errores CIRCUIT BREAK
+//Patrón de Retry, de reintentos. Se estipula cuántos intentos y cada cuánto tiempo
+//Usar el Circuit Break y el patrón Retry combinados.
+//Cuidado con el uso de estos ya que ralentiza la app.
+
+try{
+    Console.WriteLine("The final name is: " + students[studentCount]);
+}catch(System.Exception ex){
+    Console.WriteLine(ex.Message);
 }
-
+//Cuidado porque mostrará al "público" el error
